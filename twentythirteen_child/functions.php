12dbaea2b816ebf30b112f0cb73488a42b49e859
nsur <surgovan@gmail.com>
2019-03-25 08:34:13 +0200

    #small code fix

--- twentythirteen_child/functions.php
+++ twentythirteen_child/functions.php
@@ -5,7 +5,6 @@ add_action('wp_enqueue_scripts', 'twentythirteen_child_theme_enqueue_scripts', 1
 function  twentythirteen_child_theme_enqueue_scripts() {
 	// Styles
 	wp_enqueue_style('parent-style', get_template_directory_uri() .'/style.css');
-	//wp_enqueue_style('style', get_stylesheet_directory_uri(). '/style.css');
 	// Scripts
 	wp_enqueue_script('landing-page-js', get_stylesheet_directory_uri(). '/js/scripts.js');
 }
